e72df7d36dcd2da0a8d4a030ae08d3a0ef782607
Emilio Bustos <emixbq@gmail.com>
2022-11-14 17:10:04 -0300

    añadir app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>HOLA, BIENVENIDO AL SERVER DE BOMBINNI</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
